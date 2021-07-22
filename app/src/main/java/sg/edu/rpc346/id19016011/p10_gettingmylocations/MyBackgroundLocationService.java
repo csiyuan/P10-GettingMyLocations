3c9e1f6e913d1e4351234486accd4d68020c7809
19016011 <19016011@rp.edu.sg>
2021-07-22 21:10:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rpc346/id19016011/p10_gettingmylocations/MyBackgroundLocationService.java
@@ -0,0 +1,73 @@
+package sg.edu.rpc346.id19016011.p10_gettingmylocations;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+
+import java.util.List;
+
+public class MyBackgroundLocationService extends Service {
+
+    private static final String TAG = MyBackgroundLocationService.class.getSimpleName();
+    private FusedLocationProviderClient client;
+
+    public MyBackgroundLocationService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: called");
+        getLocationUpdates();
+        return START_STICKY;
+    }
+
+    private void getLocationUpdates(){
+        LocationRequest mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setSmallestDisplacement(100);
+
+        mLocationRequest.setMaxWaitTime(15 * 1000);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+//        client.requestLocationUpdates(mLocationRequest, new LocationCallback(){
+//            @Override
+//            public void onLocationResult(LocationResult locationResult) {
+//                if(locationResult == null){
+//                    Log.d(TAG, "onLocationResult: location error");
+//                    return;
+//                }
+//
+//                List<Location> locations = locationResult.getLocations();
+//                LocationResult helper = new LocationResult(getApplicationContext(), locations);
+//            }
+//        });
+
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
+    }
+}
